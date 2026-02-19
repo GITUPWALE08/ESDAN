aef183fcc5f7179bc2b89406c6e8ab5718fbe0b5
eastcoast <princeadewale391@gmail.com>
2026-02-19 16:03:27 +0100

    added back the product page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import About from './pages/about';
 import Projects from './pages/projects';
 import Membership from './pages/membership';
 import Partners from './pages/partners';
-// import Products from './pages/products';
+import Products from './pages/products';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
-        {/* <Route path="products" element={<Products />} /> */}
+        <Route path="products" element={<Products />} />
         <Route path="projects" element={<Projects />} />
         <Route path="membership" element={<Membership />} />
         <Route path="partners" element={<Partners />} />
